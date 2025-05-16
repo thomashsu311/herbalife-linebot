4c3959123e7b066718abf839725b963094b74134
thomashsu311 <68890160+thomashsu311@users.noreply.github.com>
2025-05-16 11:29:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('/form-liff')
-
-@app.route('/form-liff')
-def form_liff():
-    return render_template('form_liff.html')
+    return render_template('form.html')
