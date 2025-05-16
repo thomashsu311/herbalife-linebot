f777f4cd6a87052a7d9ac660924539a8ccda5fc1
thomashsu311 <68890160+thomashsu311@users.noreply.github.com>
2025-05-16 12:02:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    return render_template('form.html')  # 指向更新後的表單
+
+if __name__ == '__main__':
+    app.run()
