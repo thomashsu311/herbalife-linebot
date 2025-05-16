c47f371e4b2d7181415b48f15dd900955f0b5693
thomashsu311 <68890160+thomashsu311@users.noreply.github.com>
2025-05-16 15:13:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('form_fresh.html')
+    return redirect('/static/form_fresh.html')
