8ea4ccde30c51f2e7afd5de8b925800fad572a97
thomashsu311 <68890160+thomashsu311@users.noreply.github.com>
2025-05-16 15:21:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, redirect
 
-app = Flask(__name__, static_url_path='/static')
+from flask import Flask, send_from_directory
 
-@app.route('/')
+app = Flask(__name__)
+
+@app.route("/")
 def index():
-    return redirect('/static/form_fresh.html')
+    return "LIFF redirect active - static test v1.0"
+
+@app.route("/form_fresh")
+def form_fresh():
+    return send_from_directory("static", "form_fresh.html")
+
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory("static", filename)
